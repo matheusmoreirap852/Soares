6893cd6bb3e2e0eb2cbc0caa02bca9ea3ad6ccb5
matheusmp852 <58013908+matheusmoreirap852@users.noreply.github.com>
2023-09-12 00:42:52 -0300

    Ajuste final

--- js/filder.js
+++ js/filder.js
@@ -16,3 +16,52 @@ $(document).ready(function () {
         return false;
     });
 });
+
+
+$(document).ready(function () {
+    // Quando o botão do menu responsivo é clicado
+    $(".navbar-toggler").click(function () {
+        // Verifica se o menu está aberto ou fechado
+        var menuAberto = $("#navbarNav").hasClass("show");
+        
+        // Se o menu estiver aberto, esconde os itens do menu
+        if (menuAberto) {
+            $(".navbar-nav").slideUp();
+        } else {
+            // Se o menu estiver fechado, mostra os itens do menu
+            $(".navbar-nav").slideDown();
+        }
+    });
+});
+
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        $('.custom-section').each(function () {
+            if ($(this).isInViewport()) {
+                $(this).animate({ opacity: 1 }, 1000);
+            }
+        });
+    });
+});
+
+// Função para verificar se um elemento está na viewport
+$.fn.isInViewport = function () {
+    var elementTop = $(this).offset().top;
+    var elementBottom = elementTop + $(this).outerHeight();
+    var viewportTop = $(window).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+    return elementBottom > viewportTop && elementTop < viewportBottom;
+};
+
+
+$(document).ready(function(){
+    $('.custom-carousel').slick({
+      autoplay: true, // Reprodução automática
+      dots: true, // Exibe pontos de navegação
+      arrows: true, // Exibe setas de navegação
+      speed: 500, // Velocidade da transição
+      slidesToShow: 3, // Número de slides visíveis ao mesmo tempo
+      slidesToScroll: 1 // Número de slides para rolar a cada transição
+    });
+  });
